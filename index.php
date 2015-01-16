ea10a8a677074a024854d776fb9f5489fa3ee08d
BobertForever <robertlynch2012@gmail.com>
2015-01-16 12:48:26 -0600

    Fixes to mainpage and addition of apply page stub

--- index.php
+++ index.php
@@ -2,18 +2,24 @@
 
 require_once('lib/OmegaCore.php');
 
+# If a user is logged in, redirect them to where they belong
 if(Session::isActive()) {
   $user = Session::getUser();
   if($user->isMember()) {
-    http_redirect('dashboard');
+    header('Location: dashboard.php');
   } else {
-    http_redirect('apply');
+    header('Location: apply.php');
   }
 }
 
 require_once('templates/header.html');
-echo "
-  <body>
-  </body>
-";
+echo '
+<body>
+  <div id="crest"></div>
+  <div id="login">
+    <button id="signin" href="login">Login</button>
+    <button id="signup">Sign Up</button>
+  </div>
+</body>
+';
 require_once('templates/footer.html');
