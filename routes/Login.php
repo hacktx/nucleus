aa34d7e984b563e0ff21a2d84aa3a3768b62f0ef
Robert Lynch <robertlynch2012@gmail.com>
2015-01-17 15:20:24 -0500

    Cleanup login page and validate input

--- routes/Login.php
+++ routes/Login.php
@@ -18,15 +18,34 @@ class Login {
     }
 
     return
-      <form method="post" action="/login">
-        <input type="text" name="username" placeholder="Username" />
-        <input type="password" name="password" placeholder="Password" />
-        <input type="checkbox" name="remember" placeholder="Remember" />
-        <button type="submit">Submit</button>
-      </form>;
+      <div class="well col-md-4 col-md-offset-4">
+        <form method="post" action="/login">
+          <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" class="form-control" id="username" name="username" placeholder="Username" />
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" id="password" name="password" placeholder="Password" />
+          </div>
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" name="remember" /> Remember Me
+            </label>
+          </div>
+          <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+      </div>;
   }
 
   public static function post(): void {
+    # Make sure all required fields were filled out
+    if(!isset($_POST['username']) || !isset($_POST['password'])) {
+      header('Location: /login');
+      return;
+    }
+
+    # Authenticate
     $remember = isset($_POST['remember']) && $_POST['remember'];
     Auth::login($_POST['username'], $_POST['password'], $remember);
 
