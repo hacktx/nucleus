7450781ae104937f872186cb1fa890c11760a9ce
Robert Lynch <robertlynch2012@gmail.com>
2015-01-21 00:41:02 -0500

    Added file exists check in autoloader
    When using an external library, many come with their own autoloaders. If
    the class we want does not exist in our code, return false from this
    autoloader so others can get a chance to run, instead of allowing this
    one to throw an error and crash the application.

--- lib/AutoLoader.php
+++ lib/AutoLoader.php
@@ -1,7 +1,11 @@
 <?hh
 
 class AutoLoader {
-  public static function loadFile($class) {
-    require_once('lib/' . $class . '.php');
+  public static function loadFile($class): bool {
+    if(file_exists('lib/' . $class . '.php')) {
+      require_once('lib/' . $class . '.php');
+      return true;
+    }
+    return false;
   }
 }
