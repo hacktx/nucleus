27feba65a1cbba262253608c15064a215494b109
Robert Lynch <robertlynch2012@gmail.com>
2015-02-02 19:54:15 -0500

    Added favicon

--- lib/Render.php
+++ lib/Render.php
@@ -23,6 +23,7 @@ class Render {
         <title>Omega | Texas LAN</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />
         <link rel="stylesheet" type="text/css" href="/css/styles.css" />
+        <link rel="icon" type="image/png" href="/img/favicon.png" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/list.js/1.1.1/list.min.js"></script>
