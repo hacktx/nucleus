245be5a273ba46a5fb67a4abd48491b32a8bbc9e
Robert Lynch <robertlynch2012@gmail.com>
2015-02-11 15:43:08 -0500

    Started abstracting page to individual XHP components

---
+++ components/head.php
@@ -0,0 +1,18 @@
+<?hh
+
+final class :omega:head extends :x:element {
+  final protected function render(): :xhp {
+    return
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Omega | Texas LAN</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />
+        <link rel="stylesheet" type="text/css" href="/css/styles.css" />
+        <link rel="icon" type="image/png" href="/img/favicon.png" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/list.js/1.1.1/list.min.js"></script>
+      </head>;
+  }
+}
--- lib/Render.php
+++ lib/Render.php
@@ -4,7 +4,7 @@ class Render {
   public static function go(:xhp $content, ?string $controller): void {
     print
       <x:frag>
-        {self::getHead()}
+        <omega:head />
         <body>
           {self::getNavbar($controller)}
           <div class="container">
@@ -13,21 +13,6 @@ class Render {
           </div>
         </body>
       </x:frag>;
-  }
-
-  private static function getHead(): :head {
-    return
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Omega | Texas LAN</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" />
-        <link rel="stylesheet" type="text/css" href="/css/styles.css" />
-        <link rel="icon" type="image/png" href="/img/favicon.png" />
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/list.js/1.1.1/list.min.js"></script>
-      </head>;
   }
 
   private static function getNavbar(?string $controller): :nav {
