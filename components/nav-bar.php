dc7074e37f0aecd31efcf31b1b5abe8fc6a5feeb
Robert Lynch <robertlynch2012@gmail.com>
2015-02-12 20:51:22 -0500

    Abstracted nav bar to its own XHP component

---
+++ components/nav-bar.php
@@ -0,0 +1,48 @@
+<?hh
+
+final class :omega:nav-bar extends :x:element {
+  attribute
+    User user,
+    string controller;
+
+  final protected function render(): :xhp {
+    $user = $this->getAttribute('user');
+    $controller = $this->getAttribute('controller');
+
+    $nav_buttons = null;
+    $login = <li><a href="/login">Login</a></li>;
+    if($user && !$user->isDisabled()) {
+      $nav_buttons = <omega:nav-buttons user={$user} controller={$controller} />;
+
+      # Logout dropdown
+      $login =
+        <li>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{$user->getUsername()} <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="/login?action=logout">Logout</a></li>
+          </ul>
+        </li>;
+    }
+
+    return
+      <nav class="navbar navbar-default navbar-static-top">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">Omega</a>
+          </div>
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            {$nav_buttons}
+            <ul class="nav navbar-nav navbar-right">
+              {$login}
+            </ul>
+          </div>
+        </div>
+      </nav>;
+  }
+}
--- lib/Render.php
+++ lib/Render.php
@@ -2,61 +2,22 @@
 
 class Render {
   public static function go(:xhp $content, ?string $controller): void {
+    $user = null;
+    if(Session::isActive()) {
+      $user = Session::getUser();
+    }
+
     print
       <x:frag>
         <omega:head />
         <body>
-          {self::getNavbar($controller)}
+          <omega:nav-bar user={$user} controller={$controller}/>
           <div class="container">
             {self::getFlash()}
             {$content}
           </div>
         </body>
       </x:frag>;
-  }
-
-  private static function getNavbar(?string $controller): :nav {
-
-    $user = null;
-    if(Session::isActive()) {
-      $user = Session::getUser();
-    }
-
-    $nav_buttons = null;
-    $login = <li><a href="/login">Login</a></li>;
-    if($user && !$user->isDisabled()) {
-      $nav_buttons = <omega:nav-buttons user={$user} controller={$controller} />;
-
-      # Logout dropdown
-      $login =
-        <li>
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{$user->getUsername()} <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="/login?action=logout">Logout</a></li>
-          </ul>
-        </li>;
-    }
-
-    return
-      <nav class="navbar navbar-default navbar-static-top">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="/">Omega</a>
-          </div>
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            {$nav_buttons}
-            <ul class="nav navbar-nav navbar-right">
-              {$login}
-            </ul>
-          </div>
-        </div>
-      </nav>;
   }
 
   private static function getFlash(): ?:div {
