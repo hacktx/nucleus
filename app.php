1a050d6216893099cf62b262bf28abb5b4090e8f
Robert Lynch <robertlynch2012@gmail.com>
2015-07-12 18:28:21 -0400

    Move to config file system

--- app.php
+++ app.php
@@ -1,27 +1,34 @@
 <?hh
 
-# Load in external libraries
+// Load in external libraries
 require 'vendor/autoload.php';
 
-# Set the app's timezone to central
+// Set the app's timezone to central
 date_default_timezone_set('America/Chicago');
 
-# Setup Mailgun and email
+if(!file_exists('config.ini')) {
+  error_log("Config file does not exist");
+  die;
+}
+
+$configs = parse_ini_file('config.ini', true);
+
+// Prepare the databae
+DB::$user = $configs['DB']['user'];
+DB::$password = $configs['DB']['password'];
+DB::$dbName = $configs['DB']['name'];
+DB::$port = $configs['DB']['port'];
+
+// Setup Mailgun and email
 use Mailgun\Mailgun;
-Email::$mg = new Mailgun('key');
-Email::$domain = 'example.com';
-Email::$from = 'hello@example.com';
+Email::$mg = new Mailgun($config['Mailgun']['key']);
+Email::$domain = $config['Mailgun']['domain'];
+Email::$from = $config['Mailgun']['from'];
 
-# Prepare the databae
-DB::$user = getenv('DB_USER');
-DB::$password = getenv('DB_PASS');
-DB::$dbName = 'omega';
-DB::$port = 3306;
-
-# Get the user session going
+// Get the user session going
 Session::init();
 
-# Call the dispatcher to do its thing
+// Call the dispatcher to do its thing
 Route::dispatch(
   parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH),
   $_SERVER['REQUEST_METHOD']
