3b471203fb1cf64aa09b574a27908ddca7a3cd47
Robert Lynch <robertlynch2012@gmail.com>
2015-07-16 16:46:13 -0400

    Use 'use' keyword for Mailgun

--- lib/email/MailgunEmailClient.php
+++ lib/email/MailgunEmailClient.php
@@ -1,12 +1,14 @@
 <?hh // strict
 
+use Mailgun\Mailgun;
+
 class MailgunEmailClient extends EmailClient {
-  private Mailgun\Mailgun $client;
+  private Mailgun $client;
   public string $domain;
   public string $email;
 
   public function __construct(string $api_key, string $domain, string $from_email): void {
-    $this->client = new Mailgun\Mailgun($api_key);
+    $this->client = new Mailgun($api_key);
     $this->domain = $domain;
     $this->email = $from_email;
   }
