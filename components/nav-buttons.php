abc81ea1564e0c23c9b2062e6dd26603958dafd5
Robert Lynch <robertlynch2012@gmail.com>
2015-08-30 23:35:41 -0400

    Only show nav buttons if the user has a proper role

--- components/nav-buttons.php
+++ components/nav-buttons.php
@@ -9,6 +9,12 @@ final class :nucleus:nav-buttons extends :x:element {
     $user = $this->getAttribute('user');
     $controller = $this->getAttribute('controller');
 
+    $roles = $user->getRoles();
+
+    if (empty($roles)) {
+      return <ul class="nav navbar-nav"></ul>;
+    }
+
     $nav_buttons =
       <ul class="nav navbar-nav">
         <li class={$controller === DashboardController::class ? 'active' : ''}>
@@ -16,6 +22,22 @@ final class :nucleus:nav-buttons extends :x:element {
         </li>
       </ul>;
 
+    if (!empty(array_intersect(array(UserRole::Organizer, UserRole::Superuser), $roles))) {
+      $nav_buttons->appendChild(
+        <li class={$controller === MembersController::class ? 'active' : ''}>
+          <a href={MembersController::getPath()}>Members</a>
+        </li>
+      );
+    }
+
+    if (in_array(UserRole::Superuser, $roles)) {
+      $nav_buttons->appendChild(
+        <li class={$controller === SettingsController::class ? 'active' : ''}>
+          <a href={SettingsController::getPath()}>Site Settings</a>
+        </li>
+      );
+    }
+    
     return $nav_buttons;
   }
 }
