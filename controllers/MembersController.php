5ab8866b6436b73aba0c171efe907cca4aba34ae
Robert Lynch <robertlynch2012@gmail.com>
2015-09-07 17:10:14 -0400

    Added in pagination to members controller

--- controllers/MembersController.php
+++ controllers/MembersController.php
@@ -6,45 +6,54 @@ class MembersController extends BaseController {
   }
 
   public static function getConfig(): ControllerConfig {
-    return (new ControllerConfig())
-      ->setUserRoles(array(UserRole::Superuser, UserRole::Organizer));
+    return (new ControllerConfig())->setUserRoles(
+      array(UserRole::Superuser, UserRole::Organizer),
+    );
   }
 
   public static function get(): :xhp {
+    $page = isset($_GET["page"]) ? (int) $_GET["page"] : 0;
+    DB::query("SELECT * FROM users");
+    $max_page = (int) (DB::count() / 25);
+
     return
       <div class="members-wrapper" role="tabpanel">
-        {self::getMembers()}
+        {self::getMembers($page, 25)}
+        {self::getPagination($page, $max_page)}
         <script src="/js/members.js"></script>
         <script src="/js/moment.min.js"></script>
         <script src="/js/bootstrap-sortable.js"></script>
       </div>;
   }
 
-  private static function getMembers(): :table {
+  private static function getMembers(int $page, int $limit): :table {
     $members = <tbody />;
 
-    $query = DB::query("SELECT * FROM users");
-    foreach($query as $row) {
-      $status = <span/>;
-      switch($row['status']) {
+    $offset = $page * $limit;
+
+    $query =
+      DB::query("SELECT * FROM users LIMIT %i OFFSET %i", $limit, $offset);
+    foreach ($query as $row) {
+      $status = <span />;
+      switch ($row['status']) {
         case UserState::Pending:
-          $status = <span>Pending<span class="pending circle"/></span>;
+          $status = <span>Pending<span class="pending circle" /></span>;
           break;
         case UserState::Accepted:
-          $status = <span>Accepted<span class="accepted circle"/></span>;
+          $status = <span>Accepted<span class="accepted circle" /></span>;
           break;
         case UserState::Waitlisted:
-          $status = <span>Waitlisted<span class="waitlisted circle"/></span>;
+          $status = <span>Waitlisted<span class="waitlisted circle" /></span>;
           break;
         case UserState::Rejected:
-          $status = <span>Rejected<span class="rejected circle"/></span>;
+          $status = <span>Rejected<span class="rejected circle" /></span>;
           break;
       }
-      
+
       // Append the row to the table
       $members->appendChild(
         <tr>
-          <td>{$row['fname'] . ' ' . $row['lname']}</td>
+          <td>{$row['fname'].' '.$row['lname']}</td>
           <td>{$row['school']}</td>
           <td>{$row['major']}</td>
           <td>{$status}</td>
@@ -65,5 +74,51 @@ class MembersController extends BaseController {
         {$members}
       </table>;
 
+  }
+
+  private static function getPagination(int $page, int $max): :nav {
+    $buttons = Vector {};
+    for (
+      $i = ($page < 2 ? 0 : $page - 2);
+      $i < ($page < 2 ? 5 : $page + 3);
+      $i++
+    ) {
+      if ($i > $max) {
+        break;
+      }
+
+      $buttons[] =
+        <li class={$i == $page ? "active" : ""}>
+          <a href={self::getPath()."?page=".$i}>{$i}</a>
+        </li>;
+    }
+
+    $back =
+      <li class={$page == 0 ? "disabled" : ""}>
+        <a
+          href={self::getPath()."?page=".($page < 5 ? 0 : $page - 5)}
+          aria-label="Previous">
+          <span aria-hidden="true">&laquo;</span>
+        </a>
+      </li>;
+
+    $next =
+      <li>
+        <a
+          href=
+            {self::getPath()."?page=".($max - $page > 5 ? $page + 5 : $max)}
+          aria-label="Next">
+          <span aria-hidden="true">&raquo;</span>
+        </a>
+      </li>;
+
+    return
+      <nav>
+        <ul class="pagination">
+          {$back}
+          {$buttons}
+          {$next}
+        </ul>
+      </nav>;
   }
 }
