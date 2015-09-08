f41c294ac87baa16b7bb1bfcf97f04f385fd5e03
Robert Lynch <robertlynch2012@gmail.com>
2015-09-07 22:01:25 -0400

    Added in feedback controller for events

---
+++ controllers/api/FeedbackApiController.php
@@ -0,0 +1,26 @@
+<?hh
+
+class FeedbackApiController extends BaseController {
+  public static function getPath(): string {
+    return '/api/feedback';
+  }
+
+  public static function post(): void {
+    if (!isset($_POST['id']) || !isset($_POST['rating'])) {
+      return;
+    }
+
+    $data = Map {'event_id' => $_POST['id'], 'rating' => $_POST['rating']};
+
+    DB::insert('feedback', $data);
+
+    $client = KeenIO\Client\KeenIOClient::factory(
+      [
+        'projectId' => Config::get('Keen')['project_id'],
+        'writeKey' => Config::get('Keen')['write_key'],
+        'readKey' => Config::get('Keen')['read_key'],
+      ],
+    );
+    $client->addEvent('event_feedback', $data->toArray());
+  }
+}
