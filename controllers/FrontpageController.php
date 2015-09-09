22ff95021a6e5667f0591048bb03e40b045ddc39
Robert Lynch <robertlynch2012@gmail.com>
2015-09-09 01:09:19 -0400

    Change button text for closed applications

--- controllers/FrontpageController.php
+++ controllers/FrontpageController.php
@@ -11,11 +11,18 @@ class FrontpageController extends BaseController {
       header('Location: /dashboard');
     }
 
+    $registration = "Registration now open";
+    $button = "APPLY NOW";
+    if (!Settings::get('applications_open')) {
+      $registration = "Registration is now closed";
+      $button = "CHECK MY STATUS";
+    }
+
     return
       <div class="col-md-6 col-md-offset-3 masthead">
         <h3 class="hero-info">Austin, TX  |  Sept. 26-27</h3>
-        <p class="prompt-open">Registration now open</p>
-        <p><a id="signin" class="btn btn-default col-xs-12" role="button" href="/login">APPLY NOW</a></p>
+        <p class="prompt-open">{$registration}</p>
+        <p><a id="signin" class="btn btn-default col-xs-12" role="button" href="/login">{$button}</a></p>
         <p class="info">Applications are acccepted on a first come first serve basis. If accepted, you will receive a confirmation email in ~7 days.</p>
         <div class="footer">
           <p class="footer-prompt">Already Have An Account? <a href={LoginController::getPath()}>Log in Here</a></p>
