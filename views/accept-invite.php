e1e5786c2af4fd791d21aebfc59dd889a03b2fef
Robert Lynch <robertlynch2012@gmail.com>
2015-09-10 00:55:34 -0400

    stylize accvept invite view

--- views/accept-invite.php
+++ views/accept-invite.php
@@ -3,15 +3,51 @@
 final class :nucleus:accept-invite extends :x:element {
   attribute User user;
 
-  final protected function render(): :div {
+  final protected function render(): XHPRoot {
     return
-      <div class="col-md-6 col-md-offset-3 text-center">
-        <form method="post" action={AcceptInviteController::getPath()}>
-          <button type="submit" class="btn btn-success" name="accept">Accept my invite!</button>
-        </form> 
-        <form method="post" action={AcceptInviteController::getPath()}>
-          <button type="submit" class="btn btn-default" name="deny">I can no longer attend</button>
-        </form> 
-      </div>;
+      <x:frag>
+        <div class="col-md-8 col-md-offset-2">
+          <h2 style="text-align: center;">Congrats! Just A Couple Things...</h2>
+          <div class="panel panel-default">
+            <div class="panel-body accept-invite-panel">
+              <h4>LIABILITY FORM</h4>
+              <p>Signing this online liability form will improve and speed up the checkin process.</p>
+              <div class="liability-btn">
+                <a href="#" class="btn btn-panel">
+                SIGN LIABILITY FORM
+                </a>
+              </div>
+
+              <h4>RESUME UPLOAD</h4>
+              <p>You are awesome and we want to show that to our sponsors. Upload your resume and we will share it with our sponsors.</p>
+              <div class="form-group">
+                <input type="file" id="resume" />
+                <a href="#" class="btn btn-panel">
+                Choose file
+                </a>
+              </div>
+
+              <h4>CODE OF CONDUCT*</h4>
+              <div class="checkbox">
+                <label><input type="checkbox" /> I will at all times abide by and conform to the <a href="http://static.mlh.io/docs/mlh-code-of-conduct.pdf">Major League Hacking Code of Conduct</a> while at HackTX.</label>
+              </div>
+            </div>
+          </div>
+          <div class="text-right">
+            <form
+              action={AcceptInviteController::getPath()}
+              method="post"
+              style="display: inline-block;">
+              <button type="submit" class="btn btn-secondary">I CANT MAKE IT</button>
+            </form>
+            <form
+              action={AcceptInviteController::getPath()}
+              method="post"
+              style="display: inline-block;">
+              <button type="submit" class="btn btn-primary">I WILL BE THERE!</button>
+            </form>
+          </div>
+        </div>
+      </x:frag>;
   }
 }
