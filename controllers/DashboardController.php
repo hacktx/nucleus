4db02d6544782780ba16c5849a545194bdc23294
Robert Lynch <robertlynch2012@gmail.com>
2015-09-14 20:28:47 -0400

    Fixed UI bug not showing Waitlisted status

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -24,38 +24,52 @@ class DashboardController extends BaseController {
     $status = UserState::getNames()[$user_status];
 
     $child = null;
-    if ($user_status == UserState::Pending) {
-      $child =
-        <x:frag>
-          <p class="info">
-            Acceptances will roll out in ~7 days. If accepted, you will receive
-            a confirmation email at {$user->getEmail()} with further
-            instructions.
-          </p>
-          <div class="footer">
-            <p>
-              {"Can't make it?"}
-              <a href={DeleteAccountController::getPath()}>
-                Cancel My Application
-              </a>
+    switch ($user_status) {
+      case UserState::Pending:
+        $child =
+          <x:frag>
+            <p class="info">
+              Acceptances will roll out in ~7 days. If accepted, you will
+              receive a confirmation email at {$user->getEmail()} with further
+              instructions.
             </p>
-          </div>
-        </x:frag>;
-    } else if ($user_status == UserState::Accepted) {
-      $child =
-        <p class="info">
-          You received an invitation! Respond to it
-          <a href={AcceptInviteController::getPath()}>here</a>
-        </p>;
-    } else if ($user_status == UserState::Confirmed) {
-      $child = <p class="info">You successfully accepted your invitation!</p>;
-    } else {
-      $child =
-        <p class="info">
-          We hope to see you next year! If you are interested in volunteering,
-          you can sign up <a href="http://goo.gl/forms/8Ygo93YMXS">here</a>
-        </p>;
-      $status = null;
+            <div class="footer">
+              <p>
+                {"Can't make it?"}
+                <a href={DeleteAccountController::getPath()}>
+                  Cancel My Application
+                </a>
+              </p>
+            </div>
+          </x:frag>;
+        break;
+      case UserState::Accepted:
+        $child =
+          <p class="info">
+            You received an invitation! Respond to it
+            <a href={AcceptInviteController::getPath()}>here</a>
+          </p>;
+        break;
+      case UserState::Waitlisted:
+        $child =
+          <p class="info">
+            Keep an eye on your email as we send out more invites! Invites are
+            awarded on a first-come-first-serve basis.
+          </p>;
+        break;
+      case UserState::Confirmed:
+        $child =
+          <p class="info">You successfully accepted your invitation!</p>;
+        break;
+      case UserState::Rejected:
+        $child =
+          <p class="info">
+            We hope to see you next year! If you are interested in
+            volunteering, you can sign up
+            <a href="http://goo.gl/forms/8Ygo93YMXS">here</a>
+          </p>;
+        $status = null;
+        break;
     }
 
     return
