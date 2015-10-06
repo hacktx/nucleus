9bccb2b0b16c88d30a5b555f6e188af3243c1e8f
Robert Lynch <robertlynch2012@gmail.com>
2015-10-06 14:35:03 -0400

    Move batch modify view out to its own file

--- controllers/members/BatchModifyController.php
+++ controllers/members/BatchModifyController.php
@@ -16,59 +16,11 @@ class BatchModifyController extends BaseController {
     foreach (UserState::getNames() as $value => $name) {
       $states[] = <option value={$name}>{$name}</option>;
     }
+
     return
-      <form method="post">
-        <div class="form-inline">
-          <div class="form-group">
-            <div class="input-group">
-              <div class="input-group-addon">Move</div>
-              <select class="form-control" name="place" id="place">
-                <option>First</option>
-                <option>Last</option>
-              </select>
-            </div>
-          </div>
-          <div class="form-group">
-            <input
-              name="number"
-              type="text"
-              class="form-control"
-              id="number-accept"
-              placeholder="600"
-            />
-          </div>
-          <div class="form-group">
-            <div class="input-group">
-              <div class="input-group-addon">From</div>
-              <select class="form-control" name="from" id="from">
-                {$states}
-              </select>
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="input-group">
-              <div class="input-group-addon">To</div>
-              <select class="form-control" name="to" id="to">
-                {$states}
-              </select>
-            </div>
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="subject">Email Subject</label>
-          <input
-            type="text"
-            class="form-control"
-            id="subject"
-            name="subject"
-          />
-        </div>
-        <div class="form-group">
-          <label for="email-input">Email Contents</label>
-          <textarea class="form-control" rows={3} name="email" />
-        </div>
-        <button type="submit" class="btn btn-default">Submit</button>
-      </form>;
+      <nucleus:batch-modify>
+        {$states}
+      </nucleus:batch-modify>;
   }
 
   public static function post(): void {
---
+++ views/batch-modify.php
@@ -0,0 +1,61 @@
+<?hh // strict
+
+final class :nucleus:batch-modify extends :x:element {
+  children (:option+);
+
+  final protected function render(): :form {
+    return 
+      <form method="post">
+        <div class="form-inline">
+          <div class="form-group">
+            <div class="input-group">
+              <div class="input-group-addon">Move</div>
+              <select class="form-control" name="place" id="place">
+                <option>First</option>
+                <option>Last</option>
+              </select>
+            </div>
+          </div>
+          <div class="form-group">
+            <input
+              name="number"
+              type="text"
+              class="form-control"
+              id="number-accept"
+              placeholder="600"
+            />
+          </div>
+          <div class="form-group">
+            <div class="input-group">
+              <div class="input-group-addon">From</div>
+              <select class="form-control" name="from" id="from">
+                {$this->getChildren()}
+              </select>
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="input-group">
+              <div class="input-group-addon">To</div>
+              <select class="form-control" name="to" id="to">
+                {$this->getChildren()}
+              </select>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="subject">Email Subject</label>
+          <input
+            type="text"
+            class="form-control"
+            id="subject"
+            name="subject"
+          />
+        </div>
+        <div class="form-group">
+          <label for="email-input">Email Contents</label>
+          <textarea class="form-control" rows={3} name="email" />
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>;
+  }
+}
