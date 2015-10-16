e7fb99fc20bde541abb7b65c70f7b76e3dd94236
Robert Lynch <robertlynch2012@gmail.com>
2015-10-16 12:48:52 -0400

    [documentation] Add doc blocks to BaseController and ControllerConfig

--- lib/BaseController.php
+++ lib/BaseController.php
@@ -1,12 +1,33 @@
 <?hh // strict
 
 abstract class BaseController {
+  /**
+   * The path which the controller will live on
+   *
+   * A controller can specify the path it is to live on by specifying which in
+   * this method. A path can be basic, such as `/example`, or use regex to
+   * specify path params, such as `/example/(?<id>\d+)`, which can be retreived
+   * using `getPathParam()`
+   */
   abstract public static function getPath(): string;
 
+  /**
+   * The configration for the controller.
+   *
+   * Controller configs can be used to setup permissions for a controller, or
+   * additional options such as the title of the page.
+   */
   public static function getConfig(): ControllerConfig {
     return (new ControllerConfig());
   }
 
+  /**
+   * Get a path param for a controller which uses a regex path
+   *
+   * When a controller specifies a regex-based path with a path param, such as
+   * `/example/(?<id>\d+)`, this function will return the value in the path
+   * with the key `id`.
+   */
   public static function getPathParam(string $key): string {
     return getRouteParams()[$key];
   }
--- lib/ControllerConfig.php
+++ lib/ControllerConfig.php
@@ -5,6 +5,12 @@ class ControllerConfig {
   private array<UserState> $user_state = array();
   private array<UserRole> $user_roles = array();
 
+  /**
+   * Set the required user states for a controller
+   *
+   * A user must have one of the states specified by this function in order to
+   * access the methods of a controller.
+   */
   public function setUserState(array<UserState> $states): this {
     $this->user_state = $states;
     return $this;
@@ -14,6 +20,12 @@ class ControllerConfig {
     return $this->user_state;
   }
 
+  /**
+   * Set the required user roles for a controller
+   *
+   * A user must have one of the roles specified by this function in order to
+   * access the methods of a controller.
+   */
   public function setUserRoles(array<UserRole> $roles): this {
     $this->user_roles = $roles;
     return $this;
@@ -23,6 +35,12 @@ class ControllerConfig {
     return $this->user_roles;
   }
 
+  /**
+   * Set the title for the controller
+   *
+   * By specifying a title with this function, when rendered, the page title
+   * will contain the title specified.
+   */
   public function setTitle(string $title): this {
     $this->title = $title;
     return $this;
