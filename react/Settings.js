d1d11241a7b5ec8ff6147291f1c0e714c8da8e9e
Thomas Gaubert <tpgaubert@gmail.com>
2016-02-14 15:50:07 -0600

    Settings are no longer read only

--- react/Settings.js
+++ react/Settings.js
@@ -1,6 +1,12 @@
 var React = require('react');
 
 class Settings extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onChange = this.onChange.bind(this);
+  }
+
   render() {
     return (
       <div className="col-md-6 col-md-offset-3">
@@ -10,7 +16,7 @@ class Settings extends React.Component {
                 <div className="form-group">
                   <div className="checkbox">
                     <label>
-                      <input type="checkbox" name="applications_disabled" checked={!this.props.settings.applications_open}/> Disable Applications
+                      <input type="checkbox" name="applications_disabled" checked={!this.props.settings.applications_open} onChange={this.onChange}/> Disable Applications
                     </label>
                   </div>
                 </div>
@@ -20,6 +26,10 @@ class Settings extends React.Component {
         </form>
       </div>
     );
+  }
+
+  onChange() {
+    this.setState({isChecked: !this.state.isChecked});
   }
 }
 
