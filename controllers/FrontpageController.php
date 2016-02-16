7f0a12b39fc21048645431f9ca9f60c771c40ad8
Thomas Gaubert <ThomasGaubert@users.noreply.github.com>
2016-02-16 12:34:59 -0600

    Applications -> Signups

--- controllers/FrontpageController.php
+++ controllers/FrontpageController.php
@@ -24,7 +24,7 @@ class FrontpageController extends BaseController {
         <h3 class="hero-info">Austin, TX  |  March 26, 2016</h3>
         <p class="prompt-open">{$registration}</p>
         <p><a id="signin" class="btn btn-default col-xs-12" role="button" href={LoginController::getPath()}>{$button}</a></p>
-        <p class="info">Applications are acccepted on a first come first serve basis. If accepted, you will receive a confirmation email in ~7 days.</p>
+        <p class="info">Signups are acccepted on a first come first serve basis. If accepted, you will receive a confirmation email in ~7 days.</p>
         <div class="footer">
           <p class="footer-prompt">Already Have An Account? <a href={LoginController::getPath()}>Log in Here</a></p>
           <p class="footer-prompt">Having Trouble? Email Us <a href="mailto:hello@freetailhackers.com">hello@freetailhackers.com</a></p>
