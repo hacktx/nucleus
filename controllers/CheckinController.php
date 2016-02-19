846302e0afd410121c4c957a6553086cc1584414
Thomas Gaubert <tpgaubert@gmail.com>
2016-02-19 17:52:35 -0600

    Fixed QR codes

--- controllers/CheckinController.php
+++ controllers/CheckinController.php
@@ -15,12 +15,13 @@ class CheckinController extends BaseController {
 
   public static function get(): :xhp {
     $user = Session::getUser();
+    $url = "http://chart.apis.google.com/chart?cht=qr&chs=150x150&chld=H|0&chl=".$user->getEmail();
 
     return
       <div>
         <h3>Check-in <small>{$user->getEmail()}</small></h3>
           <p>Use this code to quickly check-in at the event.</p>
-          <img class="img-responsive center-block" src="http://chart.apis.google.com/chart?cht=qr&chs=150x150&chl={$user->getEmail()} &chld=H|0"/>
+          <img class="img-responsive center-block" src={$url}/>
           <a class="btn btn-primary" style="margin-top: 20px;" href={DashboardController::getPath()}>Back</a>
       </div>;
   }
